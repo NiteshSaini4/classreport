32e54886584fb96a3de0bf103d5dc8c9163c6fed
unknown <niteshqatester4@gmail.com>
2025-12-29 10:24:41 +0530

    class Name change

---
+++ src/main/java/com/dmct/Class3Pm/App.java
@@ -0,0 +1,13 @@
+package com.dmct.Class3Pm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
